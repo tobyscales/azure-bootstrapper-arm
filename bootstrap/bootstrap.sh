ff2231dace2786d8811eeb186197665d98e056d8
tescales <toby.scales@microsoft.com>
2019-09-09 11:05:14 -0700

    update with --no-self-perms

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -6,5 +6,6 @@ az login --identity
 az configure --defaults location=$AZURE_LOCATION
 az configure --defaults group=$AZURE_RESOURCE_GROUP
 
-## sample creation
-az keyvault create --name kv-$AZURE_RESOURCE_GROUP --enabled-for-deployment true --enabled-for-template-deployment true 
+## sample keyvault creation, using --no-self-perms since we are running under a managed identity
+az keyvault create --name kv-$AZURE_RESOURCE_GROUP --enabled-for-deployment true --enabled-for-template-deployment true --no-self-perms
+
