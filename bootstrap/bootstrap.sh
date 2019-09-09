1cd09ec4f271ef4226b08f6b08c48a07ac2c98ec
tescales <toby.scales@microsoft.com>
2019-09-09 11:16:01 -0700

    add sample jq output to bootstrap script

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -8,5 +8,5 @@ az configure --defaults location=$AZURE_LOCATION
 az configure --defaults group=$AZURE_RESOURCE_GROUP
 
 ## sample keyvault creation, using --no-self-perms since we are running under a managed identity
-az keyvault create --name kv-$AZURE_RESOURCE_GROUP --enabled-for-deployment true --enabled-for-template-deployment true --no-self-perms
-
+az keyvault create --name kv-$AZURE_RESOURCE_GROUP --enabled-for-deployment true --enabled-for-template-deployment true --no-self-perms > output.json
+jq -r '"keyVaultUrl --value \(.vaultUri)"' output.json
