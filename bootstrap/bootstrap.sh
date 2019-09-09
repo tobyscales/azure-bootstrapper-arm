3cfd4ed8c9f39b6cc7703d2cc849d131aa6ab490
tescales <toby.scales@microsoft.com>
2019-09-09 11:48:15 -0700

    update boot

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -7,11 +7,11 @@ az login --identity
 az configure --defaults location=$AZURE_LOCATION
 az configure --defaults group=$AZURE_RESOURCE_GROUP
 
-## sample keyvault creation requires --no-self-perms since we are running under a managed identity
+## sample keyvault creation (requires --no-self-perms since we are running under a managed identity)
 az keyvault create --name kv-$AZURE_RESOURCE_GROUP --enabled-for-deployment true --enabled-for-template-deployment true --no-self-perms > output.json
 jq -r '"keyVaultUrl: \(.properties.vaultUri)"' output.json
 
 az keyvault secret set --name "mySecret" --value "mySecretValue" --vault-name kv-$AZURE_RESOURCE_GROUP > secret.json
 cat secret.json
 
-tail /dev/null
+#tail -f /dev/null
