4db9b0282af0b7b2139077dbe50925e3fbaca980
tescales <toby.scales@microsoft.com>
2019-09-09 11:58:53 -0700

    update bootstrap.sh with comments

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -8,10 +8,11 @@ az configure --defaults location=$AZURE_LOCATION
 az configure --defaults group=$AZURE_RESOURCE_GROUP
 
 ## sample keyvault creation (requires --no-self-perms since we are running under a managed identity)
-az keyvault create --name kv-$AZURE_RESOURCE_GROUP --enabled-for-deployment true --enabled-for-template-deployment true --no-self-perms > output.json
-jq -r '"keyVaultUrl: \(.properties.vaultUri)"' output.json
+#az keyvault create --name kv-$AZURE_RESOURCE_GROUP --enabled-for-deployment true --enabled-for-template-deployment true --no-self-perms > output.json
+#jq -r '"keyVaultUrl: \(.properties.vaultUri)"' output.json
 
 az keyvault secret set --name "mySecret" --value "mySecretValue" --vault-name kv-$AZURE_RESOURCE_GROUP > secret.json
-cat secret.json
+jq -r '"secretUrl: \(.attributes.id)"' secret.json
 
+## uncomment the below statement to troubleshoot your startup script interactively in ACI
 #tail -f /dev/null
