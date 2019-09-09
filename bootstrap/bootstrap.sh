46ed2613c498f785ca2e33b6ee53bc33fe72d9cc
tescales <toby.scales@microsoft.com>
2019-09-09 12:46:03 -0700

    cleanup bootstrap code

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -7,16 +7,12 @@ az login --identity
 az configure --defaults location=$AZURE_LOCATION
 az configure --defaults group=$AZURE_RESOURCE_GROUP
 
-### sample keyvault creation (requires --no-self-perms since we are running under a managed identity)
-#az keyvault create --name kv-$AZURE_RESOURCE_GROUP --enabled-for-deployment true --enabled-for-template-deployment true --no-self-perms > output.json
-#jq -r '"keyVaultUrl: \(.properties.vaultUri)"' output.json
-
 ## sample keyvault secret set/get operations
-az keyvault secret set --name "mySecret" --value "mySecretValue" --vault-name $AZURE_KEYVAULT > secret.json
+az keyvault secret set --vault-name $AZURE_KEYVAULT --name "mySecret" --value "mySecretValue"  > secret.json
 jq -r '"secretUrl: \(.id)"' secret.json
 
-az keyvault secret show --vault-name $AZURE_KEYVAULT --name "mySecret" > secret.json
-jq -r '"secretValue: \(.value)"' secret.json
+az keyvault secret show --vault-name $AZURE_KEYVAULT --name "mySecret" > value.json
+jq -r '"secretValue: \(.value)"' value.json
 
-## uncomment the below statement to troubleshoot your startup script interactively in ACI
-tail -f /dev/null
+## uncomment the below statement to troubleshoot your startup script interactively in ACI (Connect tab)
+#tail -f /dev/null
