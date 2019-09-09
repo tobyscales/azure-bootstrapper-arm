8017a98c7b96a512112221de2fbf3b7467f7e388
tescales <toby.scales@microsoft.com>
2019-09-09 12:42:29 -0700

    bootstrap test

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -13,10 +13,10 @@ az configure --defaults group=$AZURE_RESOURCE_GROUP
 
 ## sample keyvault secret set/get operations
 az keyvault secret set --name "mySecret" --value "mySecretValue" --vault-name $AZURE_KEYVAULT > secret.json
-jq -r '"secretUrl: \(.attributes.id)"' secret.json
+jq -r '"secretUrl: \(.id)"' secret.json
 
 az keyvault secret show --vault-name $AZURE_KEYVAULT --name "mySecret" > secret.json
 jq -r '"secretValue: \(.value)"' secret.json
 
 ## uncomment the below statement to troubleshoot your startup script interactively in ACI
-#tail -f /dev/null
+tail -f /dev/null
