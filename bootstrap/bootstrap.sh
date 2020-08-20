6270117d0c3cab0647e05085c1d1aab9c05bc9fd
tescales <toscal@microsoft.com>
2020-08-19 17:44:10 -0700

    add support for external deployment

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 # additional environment variables available: $AZURE_SUBSCRIPTION_ID, $AZURE_AADTENANT_ID and $AZURE_KEYVAULT
 
-
 echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 echo You\'re running the default bootstrap.sh script. 
 echo To execute a custom script, provide either a bootStrapUri 
 echo or gitHubRepo and gitHubUsername as parameters.
-echo '''''''''&&&&&&&&&&&&&&&&&&&***********######'#####'###########
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 ## sample keyvault secret set/get operations
 az keyvault secret set --vault-name $AZURE_KEYVAULT --name "mySecret" --value "mySecretValue"  > secret.json
